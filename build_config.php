49ce08589adb2a2a445f5b26a309c6f11d3a0ffc
Aaron Crawford <info@alleghenycreative.com>
2019-06-20 13:31:22 -0400

    Update to write UI options to config file.

--- build_config.php
+++ build_config.php
@@ -5,6 +5,9 @@
 * the requiried INI format and write the config file to the appropriate location with the correct naming.
 */
 
+$options = unserialize($cur_mod['moduleOptions']);
+$base_gpio_path = '/sys/class/gpio/gpio';
+
 // Build Config
 $module_config_array['Module'.$cur_mod['svxlinkName']] = [
 	'NAME' => $cur_mod['svxlinkName'],
@@ -14,40 +17,31 @@ $module_config_array['Module'.$cur_mod['svxlinkName']] = [
 
 # MODE: Select the operating mode "FOLLOW_PTT" or "COUNT_DOWN"
 $module_config_array['Module'.$cur_mod['svxlinkName']] += [
-	'MODE' => 'FOLLOW_PTT',
-	'DELAY' => '1',
+	'MODE' => $options['mode'],
+	'DELAY' => $options['delay'],
 ];
 
 # Path in the file system where the digital inputs can be monitored
 # 2 paths are required, if there is only 1 PTT, assign them the same GPIO.
 $module_config_array['Module'.$cur_mod['svxlinkName']] += [
-	'PTT_PATH_1' => '/sys/class/gpio/gpio506/value',
-	'PTT_PATH_2' => '/sys/class/gpio/gpio506/value',
+	'PTT_PATH_1' => $base_gpio_path . trim($options['ptt1_gpio']) . '/value',
 ];
+
+if(trim($options['ptt2_gpio']) != '') {
+	$module_config_array['Module'.$cur_mod['svxlinkName']] += [
+		'PTT_PATH_2' => $base_gpio_path . trim($options['ptt2_gpio']) . '/value',
+	];	
+} else {
+	// Set PTT2 to same value as PTT1
+	$module_config_array['Module'.$cur_mod['svxlinkName']] += [
+		'PTT_PATH_2' => $base_gpio_path . trim($options['ptt1_gpio']) . '/value',
+	];	
+}
+
 
 # Fan GPIO
 $module_config_array['Module'.$cur_mod['svxlinkName']] += [
-	'FAN_GPIO' => '/sys/class/gpio/gpio507/value',
+	'FAN_GPIO' => $base_gpio_path . trim($options['fan_gpio']) . '/value',
 ];
 
-
-/*
-[ModuleTxFan]
-NAME=TxFan
-PLUGIN_NAME=Tcl
-
-#Select the operating mode "FOLLOW_PTT" or "COUNT_DOWN"
-#MODE="FOLLOW_PTT"
-MODE="COUNT_DOWN"
-
-#Number of seconds to delay during COUNT_DOWN mode
-DELAY=10
-
-# Path in the file system where the digital inputs can be monitored
-# 2 paths are required, if there is only 1 PTT, assign them the same GPIO.
-PTT_PATH_1="/sys/class/gpio/gpio506/value"
-PTT_PATH_2="/sys/class/gpio/gpio507/value"
-
-FAN_GPIO="/sys/class/gpio/gpio497/value"
-*/
 ?>
